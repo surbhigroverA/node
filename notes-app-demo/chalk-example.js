b0c61b140fad0701ccd6a3256df2528f581a9571
Surbhi Grover <surbhi.grover@ucdconnect.ie>
2019-10-31 01:57:32 +0000

    Importing npm modules

---
+++ notes-app-demo/chalk-example.js
@@ -0,0 +1,12 @@
+const chalk = require('chalk')
+
+console.log(chalk.green('Sucess!!'));
+console.log(chalk.red("Failure!!"))
+
+console.log(chalk.yellow.underline.bold("Surbhi!!"))
+
+console.log(chalk.white.underline.inverse("Grover!!"))
+
+
+const msg = chalk.magenta.bold("Demo")
+console.log(msg)
---
+++ notes-app-demo/validator_example.js
@@ -0,0 +1,6 @@
+const validator = require('validator')
+
+console.log(validator.isEmail('surbhi.grover@ucdconnect.ie'))
+console.log(validator.isEmail('ucd.ie'))
+
+console.log(validator.isURL('https://google.ie'))
